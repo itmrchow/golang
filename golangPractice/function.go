53770c861f35ef80b62a9f88eb5a802f9c3b3d14
Jeff <itmrchow@gmail.com>
2020-09-22 15:23:20 +0800

    不定長入參

--- golangPractice/function.go
+++ golangPractice/function.go
@@ -7,3 +7,12 @@ func hello(name string) (bool, string) {
 
 	return true, "success"
 }
+
+func sum(args ...float64) float64 {
+	sum := 0.0
+	for _, v := range args {
+		sum += v
+	}
+
+	return sum
+}
