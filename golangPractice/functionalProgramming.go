df0877f057b4bf5be899943b7546d2fcbda582ce
Jeff <itmrchow@gmail.com>
2020-09-22 21:25:14 +0800

    高階函式

--- golangPractice/functionalProgramming.go
+++ golangPractice/functionalProgramming.go
@@ -5,3 +5,14 @@ import "fmt"
 func funcTest(msg string) {
 	fmt.Println(msg)
 }
+
+//方法作為參數
+func apply(arr []float64, callback func(float64) float64) []float64 {
+	out := make([]float64, len(arr))
+
+	for i, v := range arr {
+		out[i] = callback(v)
+	}
+
+	return out
+}
--- golangPractice/main.go
+++ golangPractice/main.go
@@ -1,7 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
 	f := funcTest
 
 	f("hihi")
+
+	arr := []float64{1, 2, 3}
+
+	//平方
+	sqr := apply(arr, func(n float64) float64 {
+		return n * n
+	})
+
+	//立方
+	cub := apply(arr, func(n float64) float64 {
+		return n * n * n
+	})
+
+	fmt.Printf("平方：%f", sqr)
+	fmt.Printf("立方：%f", cub)
 }
