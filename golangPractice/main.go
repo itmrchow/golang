3c37c680022fb69df1749c14828414f07cc0995f
Jeff <itmrchow@gmail.com>
2020-09-22 14:20:34 +0800

    動態配置指標

--- golangPractice/main.go
+++ golangPractice/main.go
@@ -1,5 +1,5 @@
 package main
 
 func main() {
-	pointerTest()
+	pointerTest2()
 }
--- golangPractice/pointer.go
+++ golangPractice/pointer.go
@@ -5,11 +5,27 @@ import (
 	"log"
 )
 
+//靜態配置
 func pointerTest() {
 	n := 2
 
 	//變數位址
 	nPtr := &n
+
+	fmt.Println(nPtr)
+
+	//取位址值
+	if !(*nPtr == 2) {
+		log.Fatal("Wrong value")
+	}
+
+}
+
+//動態配置
+func pointerTest2() {
+	//變數位址
+	nPtr := new(int)
+	*nPtr = 2
 
 	fmt.Println(nPtr)
 
