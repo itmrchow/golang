bec7670433769cff8839f23e63778cf41702b5a2
Jeff <itmrchow@gmail.com>
2020-09-22 13:59:35 +0800

    匿名struct

--- golangPractice/struct.go
+++ golangPractice/struct.go
@@ -10,8 +10,14 @@ type Point struct {
 
 func structTest() {
 	p := Point{x: 3.0, y: 4.0}
+	agent := struct {
+		name string
+		age  int
+	}{name: "Jeff", age: 27}
 
 	fmt.Println(p.x)
 	fmt.Println(p.y)
+	fmt.Println(agent.name)
+	fmt.Println(agent.age)
 
 }
