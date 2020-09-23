ec59b837f4dc642564ad0b0c524cfd8102d37a36
Jeff <itmrchow@gmail.com>
2020-09-23 11:07:38 +0800

    map

--- golangPractice/higherOrderFunction.go
+++ golangPractice/higherOrderFunction.go
@@ -11,3 +11,13 @@ func filter(arr []int, predicate func(int) bool) []int {
 	}
 	return out
 }
+
+//map 讓每個元素經過函式轉換
+func mapply(arr []int, mapper func(int) int) []int {
+	//out := make([]int, 0)
+
+	for i, v := range arr {
+		arr[i] = mapper(v)
+	}
+	return arr
+}
--- golangPractice/main.go
+++ golangPractice/main.go
@@ -4,10 +4,15 @@ import "fmt"
 
 func main() {
 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	out := filter(arr, func(n int) bool {
+	filterOut := filter(arr, func(n int) bool {
 		return n%2 == 0
 	})
 
-	fmt.Println(out)
+	mapOut := mapply(arr, func(n int) int {
+		return n * 5
+	})
+
+	fmt.Printf("filterOut:%v\n", filterOut)
+	fmt.Printf("mapOut:%v\n", mapOut)
 
 }
