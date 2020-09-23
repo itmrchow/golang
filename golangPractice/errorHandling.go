c12089f4a5641acb322422ea27656c4a0f2001ea
Jeff <itmrchow@gmail.com>
2020-09-23 15:04:24 +0800

    嚴重錯誤

--- golangPractice/errorHandling.go
+++ golangPractice/errorHandling.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
 func errorHandling() {
-	err := errors.New("some error")
-	//err不為空
-	if err != nil {
-		fmt.Println(err)
-	}
+	panic("Some error")
 
-	//一般錯誤發生程式不停止，嚴重錯誤才會停
+	// It didn't occur.
 	fmt.Println("More message")
 }
